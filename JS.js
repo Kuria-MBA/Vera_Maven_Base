526b98d9c4cf26cce2b50da113c426702d469e5c
Kuria-MBA <87766690+Kuria-MBA@users.noreply.github.com>
2023-05-14 21:29:02 -0400

    Add files via upload
    Added Stuff

--- JS.js
+++ JS.js
@@ -1,7 +1,30 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Function to check if the user has scrolled to the bottom
+function isAtBottom() {
+  return (
+    window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
+  );
+}
+
+// Function to toggle the visibility of the bottom div
+function toggleBottomDiv() {
+  var bottomDiv = document.getElementById("bottomDiv");
+  if (isAtBottom()) {
+    bottomDiv.style.bottom = "0"; // Show the div
+  } else {
+    bottomDiv.style.bottom = "-400px"; // Hide the div
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+}
+
+// Event listener for scroll events
+window.addEventListener("scroll", toggleBottomDiv);
+
+// Manually trigger the visibility of bottom div on page load
+window.addEventListener("load", toggleBottomDiv);
